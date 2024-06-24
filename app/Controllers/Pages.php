6cf59f5caf9b528e363c70cba36eb55d57557d49
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-24 10:50:18 -0300

    feat: add rotas para o controller Pages + criando controller Pages

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+	public function index() 
+	{
+		//
+	}
+
+	public function view(string $page = 'home')
+	{
+		//
+	}
+}
