1e7640a641045cf90a4de4dcf9cf2be16ce66176
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-24 11:00:34 -0300

    feat: add paginas Home e about + logica do metodo Pages::view

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Exceptions\PageNotFoundException;
+
 class Pages extends BaseController
 {
 	public function index() 
@@ -11,6 +13,15 @@ class Pages extends BaseController
 
 	public function view(string $page = 'home')
 	{
-		//
+		if (! is_file(APPPATH . 'Views/pages/' . $page . '.php')) {
+            // Whoops, we don't have a page for that!
+            throw new PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        return view('templates/header', $data)
+            . view('pages/' . $page)
+            . view('templates/footer');
 	}
 }
