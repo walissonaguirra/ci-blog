1f23defa71f879ca49735fd9db452f13c142d0ba
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-24 11:48:35 -0300

    fix: corrige passagem de paramento na rota

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,7 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 $routes->get('news', 'News::index');
-$routes->get('news/(:segment)', 'News::show');
+$routes->get('news/(:segment)', 'News::show/$1');
 
 $routes->get('pages', 'Pages::index');
 $routes->get('(:segment)', 'Pages::view');
