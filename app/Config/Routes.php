6af1628bdb5c9e8d161ba2813aaa4a98071fa73a
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-24 11:41:50 -0300

    feat: add Controllers\News + add Rotas para News + add Views para listagem de news

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,5 +7,8 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+$routes->get('news', 'News::index');
+$routes->get('news/(:segment)', 'News::show');
+
 $routes->get('pages', 'Pages::index');
 $routes->get('(:segment)', 'Pages::view');
---
+++ app/Controllers/News.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\News as NewsModel;
+
+class News extends BaseController
+{
+    public function index()
+    {
+        $model = model(NewsModel::class);
+        
+        $data = [
+            'news_list' => $model->getNews(),
+            'title'     => 'News archive',
+        ];
+
+        return view('templates/header', $data)
+            . view('news/index')
+            . view('templates/footer');
+    }
+
+    public function show(?string $slug = null)
+    {
+        $model = model(NewsModel::class);
+
+        $data['news'] = $model->getNews($slug);
+    }
+}
