eab720b90517ec1e209f251cf7dcd4761d7f39ef
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-24 12:21:37 -0300

    feat: add fields permitidos

--- app/Models/News.php
+++ app/Models/News.php
@@ -12,7 +12,7 @@ class News extends Model
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['title', 'slug', 'body'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
