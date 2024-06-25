985d93d1d5f4056bd6a21fe1b0e4b6bf02c305a1
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-25 03:10:16 -0300

    feat: redirecionando rota `/` para `/posts`

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,6 +3,8 @@
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\Post;
 
+$routes->get('/', fn () => redirect('posts'));
+
 $routes->get('posts', [Post::class, 'list']);
 $routes->post('posts', [Post::class, 'store']);
 $routes->get('posts/(:segment)', [Post::class, 'show']);
