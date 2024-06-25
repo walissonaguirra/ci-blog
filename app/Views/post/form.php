f8bb00ee16c715ae7dae55a45ed148785e3711ad
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-25 04:11:35 -0300

    feat: estilizando form de cadastro com bootstrap

--- app/Views/post/form.php
+++ app/Views/post/form.php
@@ -1,15 +1,26 @@
 <?= $this->extend('layout') ?>
 <?= $this->section('content') ?>
 
-	<?= validation_list_errors() ?>
-	<?= form_open('/posts') ?>
-		<label>Title:</label>
-		<input type="text" name="title" value="<?= set_value('title') ?>" autofocus/>
+	<div class="container py-2">
+		<div class="mb-2 text-end">
+			<a class="btn btn-light" href="<?= previous_url() ?>"><i class="bi bi-arrow-return-left"></i></a>
+		</div>
 
-		<label>Content:</label>
-		<input type="text" name="content" value="<?= set_value('content') ?>" />
+		<?= validation_list_errors() ?>
+		<?= form_open('/posts') ?>
 
-		<button type="submit">save</button>
-	<?= form_close() ?>
+			<div class="mb-3">
+				<label class="form-label">Título</label>
+				<input class="form-control" type="text" id="title" name="title" value="<?= set_value('title') ?>" autofocus/>
+			</div>
+
+			<div class="mb-3">
+				<label class="form-label">Conteúdo</label>
+				<textarea class="form-control" id="content" name="content" rows="3"><?= set_value('content') ?></textarea>
+			</div>
+
+			<button class="btn btn-primary col-12 col-md-3" type="submit">Salvar</button>
+		<?= form_close() ?>
+	</div>
 
 <?= $this->endSection() ?>
