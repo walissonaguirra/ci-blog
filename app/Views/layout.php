09de5699e0309245e33d908435374867bbbf42d6
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-25 03:54:11 -0300

    feat: add bootstrap icons

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?= link_tag('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css') ?>
+  <?= link_tag('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css') ?>
 </head>
 <body>
   <?= $this->renderSection('content') ?>
