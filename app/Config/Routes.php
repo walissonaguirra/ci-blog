991d3126750d97f9d7974f5df424001eefb95b2c
Walisson Aguirra <walissonaguirra@icloud.com>
2024-06-25 03:05:08 -0300

    feat: remove rotas desnecessarias

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -3,18 +3,6 @@
 use CodeIgniter\Router\RouteCollection;
 use App\Controllers\Post;
 
-/**
- * @var RouteCollection $routes
- */
 $routes->get('posts', [Post::class, 'list']);
 $routes->post('posts', [Post::class, 'store']);
 $routes->get('posts/(:segment)', [Post::class, 'show']);
-
-
-$routes->get('news', 'News::index');
-$routes->get('news/new', 'News::new');
-$routes->post('news', 'News::create');
-$routes->get('news/(:segment)', 'News::show/$1');
-
-$routes->get('pages', 'Pages::index');
-$routes->get('(:segment)', 'Pages::view');
